78b6038f95a76e093697d713f2f801131e31231e
Jmarquez03 <116291051+Jmarquez03@users.noreply.github.com>
2024-10-12 12:59:25 -0500

    Add project files.

---
+++ AdvisingAssistant/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using AdvisingAssistant.Models;
+
+namespace AdvisingAssistant.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<AdvisingAssistant.Models.Course> Course { get; set; } = default!;
+    }
+}
