afc3539798e918465c85ebc95dfe2a476b804ec1
Jmarquez03 <116291051+Jmarquez03@users.noreply.github.com>
2024-12-01 03:51:33 -0600

    Initialize test with new changes.

---
+++ AdvisingAssistant/Data/Migrations/20241014010512_Course.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AdvisingAssistant.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class Course : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "CourseID",
+                table: "Course",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CourseID",
+                table: "Course");
+        }
+    }
+}
---
+++ AdvisingAssistant/Data/Migrations/20241201074912_InitialCreate.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AdvisingAssistant.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "CourseNumber",
+                table: "Course",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Prerequisite1",
+                table: "Course",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Prerequisite2",
+                table: "Course",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Subject",
+                table: "Course",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CourseNumber",
+                table: "Course");
+
+            migrationBuilder.DropColumn(
+                name: "Prerequisite1",
+                table: "Course");
+
+            migrationBuilder.DropColumn(
+                name: "Prerequisite2",
+                table: "Course");
+
+            migrationBuilder.DropColumn(
+                name: "Subject",
+                table: "Course");
+        }
+    }
+}
